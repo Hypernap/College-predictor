88ff4795141f851e316c79e81473d1746e8944f1
Hypernap <112685230+Hypernap@users.noreply.github.com>
2022-10-26 07:06:28 +0000

    UI fix and Fixing the routing for collage webpage

--- app1/views.py
+++ app1/views.py
@@ -7,8 +7,8 @@ from django.http import HttpResponse, HttpResponseRedirect
 
 
 def home(request):
-  if request.method == "GET" and request.GET and request.GET.get(
-      'q').isdigit() and 0 <= int(request.GET.get('q')) <= 100:
+  if request.method == "GET" and request.GET and isfloat(
+      request.GET.get('q')) and 0 <= float(request.GET.get('q')) <= 100:
     p = redirect('main')
     p['location'] += '?q=' + request.GET.get('q')
     return p
@@ -28,14 +28,35 @@ def main(request):
   if request.method == "GET" and request.GET and isfloat(
       request.GET.get('q')) and 0 <= float(request.GET.get('q')) <= 100:
     q = float(request.GET.get('q'))
-    context['cs_clgs'] = Collagename.objects.filter(
-      cs_cutoff__range=[0, q + 1]).order_by('-cs_cutoff')
-    context['ecs_clgs'] = Collagename.objects.filter(
-      ecs_cutoff__range=[0, q + 1]).order_by('-ecs_cutoff')
-    context['extc_clgs'] = Collagename.objects.filter(
-      extc_cutoff__range=[0, q + 1]).order_by('-extc_cutoff')
-    context['mech_clgs'] = Collagename.objects.filter(
-      mech_cutoff__range=[0, q + 1]).order_by('-mech_cutoff')
+    context['clgs'] = {}
+    for clg in Collagename.objects.filter(
+        cs_cutoff__range=[0, q + 1]).order_by('-cs_cutoff'):
+      if clg not in context['clgs']:
+        context['clgs'][clg] = ["CS"]
+      else:
+        context['clgs'][clg].append("CS")
+
+    for clg in Collagename.objects.filter(
+        ecs_cutoff__range=[0, q + 1]).order_by('-ecs_cutoff'):
+      if clg not in context['clgs']:
+        context['clgs'][clg] = ["ECS"]
+      else:
+        context['clgs'][clg].append("ECS")
+
+    for clg in Collagename.objects.filter(
+        extc_cutoff__range=[0, q + 1]).order_by('-extc_cutoff'):
+      if clg not in context['clgs']:
+        context['clgs'][clg] = ["EXTC"]
+      else:
+        context['clgs'][clg].append("EXTC")
+
+    for clg in Collagename.objects.filter(
+        mech_cutoff__range=[0, q + 1]).order_by('-mech_cutoff'):
+      if clg not in context['clgs']:
+        context['clgs'][clg] = ["MECH"]
+      else:
+        context['clgs'][clg].append("MECH")
+    context['clgs'] = context['clgs'].items()
   return render(request, "mainpage.html", context)
 
 
